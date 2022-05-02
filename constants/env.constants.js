65d40e9137d41d91323c572eed4d2b4b9eb0fb36
mauro sala <mauro.sala@gmail.com>
2022-05-02 15:27:05 +0200

    feat(chart): deploy

--- constants/env.constants.js
+++ constants/env.constants.js
@@ -3,7 +3,6 @@ module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   COOKIE_SECRET: process.env.COOKIE_SECRET || 'krateo-platformops',
   COOKIE_NAME: process.env.COOKIE_NAME || 'krateo-platformops',
-  NATS_URI: process.env.NATS_URI,
   APP_URI: process.env.APP_URI,
   DATASTORE_URI: process.env.DATASTORE_URI,
   DEPLOYMENT_URI: process.env.DEPLOYMENT_URI,
