e74b5b4cc4fb099b4823d5cae02e547e03a4a304
mauro sala <mauro.sala@gmail.com>
2022-05-18 15:49:58 +0200

    feat(controllers): plugin post

---
+++ controllers/deployment/all.deployment.controller.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const { envConstants } = require('../../constants')
+const axiosInstance = require('../../axios-conf')
+const uriHelpers = require('../../helpers/uri.helpers')
+const { logger } = require('../../helpers/logger.helpers')
+
+router.all('/:id/plugins/:plugin/:name', async (req, res, next) => {
+  try {
+    const url = new URL(
+      uriHelpers.concatUrl([
+        envConstants.DEPLOYMENT_URI,
+        req.params.id,
+        'plugins',
+        req.params.plugin,
+        req.params.name
+      ])
+    )
+    Object.keys(req.query).forEach((key) =>
+      url.searchParams.append(key, req.query[key])
+    )
+    logger.debug(`GET ${url.toString()}`)
+
+    const plugin = await axiosInstance({
+      method: req.method,
+      url: url.toString(),
+      data: req.body
+    })
+    res.status(200).json(plugin.data)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
